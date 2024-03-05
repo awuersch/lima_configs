0f354b6592a4371b0904a88aeb5498055964504c
Tony Wuersch <twuersch@omnifederal.com>
2024-03-05 10:52:55 -0500

    added ghcrio to registry-ip-addresses.sh

--- lr/registry-ip-addresses.sh
+++ lr/registry-ip-addresses.sh
@@ -2,7 +2,7 @@
 set -euf -o pipefail
 
 # get registry ip addresses
-for name in us-docker-pkg-dev k8sio gcrio quayio dockerio; do
+for name in us-docker-pkg-dev k8sio gcrio quayio dockerio ghcrio; do
   echo -n "${name} "
   docker container inspect registry-${name} | jq -r '.[].NetworkSettings.Networks.kind.IPAddress'
 done
