f51dd47ecec4ce762eee9e5a77d92739926f449c
Tony Wuersch <twuersch@omnifederal.com>
2024-03-18 10:44:54 -0400

    update for mkfs

--- lr/install-all.sh
+++ lr/install-all.sh
@@ -9,8 +9,21 @@ echo create dirs
 mkdir -p /tmp/lima
 echo make
 (cd lima; make clean; make)
+if [[ -d ~/workspae/volumes/kind-worker ]]; then
+  DO_MKFS=true
+else
+  DO_MKFS=false
+  echo creating /tmp/lima ~/workspace/opt/lima
+  mkdir -p /tmp/lima ~/workspace/opt/lima
+  echo building volumes
+  bash -x ./setup-volumes.sh > setup-volumes.out 2>&1
 echo install
 bash -x ./install.sh $VM 2>&1 | tee install-${VM}.out
+if [[ "${DO_MKFS}" == "true" ]]; then
+  export LIMA_INSTANCE=lr0 CLUSTER=1-cilium
+  . ./source-env.sh
+  limactl shell --workdir /workdir --shell ./mkfs-volumes.sh lr0
+fi
 echo install-kind
 bash -x ./install-kind.sh $VM $CL 2>&1 | tee install-kind-${VM}-${CL}.out
 echo install-kube-prometheus-stack
--- lr/install-loki.sh
+++ lr/install-loki.sh
@@ -17,23 +17,24 @@ KUBE_CONTEXT=kind-$CLUSTER
 
 # set up loki and promtail
 
-CHART_PATH=grafana
+CHARTS_PATH=helm-charts
 CHART_DOMAIN=grafana.github.io
-CHART_URL=https://$CHART_DOMAIN/$CHART_PATH
+CHART_URL=https://$CHART_DOMAIN/$CHARTS_PATH
 CHART_REPO=grafana
-CHART_NAME=loki
 CHART_NS=loki
 
 helm repo --kube-context $KUBE_CONTEXT add $CHART_REPO $CHART_URL
 helm repo update --kube-context $KUBE_CONTEXT $CHART_REPO
-for HELM_NAME in loki promtail; do
+for CHART_NAME in loki promtail; do
   helm upgrade --install \
-    $HELM_NAME $CHART_REPO/$CHART_NAME \
-    --kube-context $KUBE_CONTEXT \
-    --namespace $CHART_NS --create-namespace
-  helm install \
-    $HELM_NAME $CHART_REPO/$CHART_NAME \
+    $CHART_NAME $CHART_REPO/$CHART_NAME \
     --kube-context $KUBE_CONTEXT \
     --namespace $CHART_NS --create-namespace \
-    --dry-run > ${HELM_NAME}-dry-run.yaml
+    --set config.logFormat=json 
+  helm install \
+    $CHART_NAME $CHART_REPO/$CHART_NAME \
+    --kube-context $KUBE_CONTEXT \
+    --namespace $CHART_NS --create-namespace \
+    --set config.logFormat=json \
+    --dry-run > ${CHART_NAME}-dry-run.yaml
 done
